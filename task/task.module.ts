cdc9586ad52ef743a9e9c62da9a4b4f34a50dc91
ImperialEEECoder <65723756+ImperialEEECoder@users.noreply.github.com>
2020-06-05 19:50:54 +0300

    Add files via upload

---
+++ task/task.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TaskPage } from './task';
+
+
+@NgModule({
+  declarations: [
+    TaskPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TaskPage),
+  ],
+  entryComponents: [
+    TaskPage
+  ]
+})
+export class TaskPageModule {}
